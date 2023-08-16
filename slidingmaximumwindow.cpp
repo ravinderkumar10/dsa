62f3a95989914ee3093b62fc393549b9340e1ccf
ravinderkumar10 <ravinderkumar18164@gmail.com>
2023-08-16 10:38:25 +0530

    slidingwindowmaximum

---
+++ slidingmaximumwindow.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> q;
+        int i=0; 
+        int j=0;
+        int n = nums.size();
+       while(j<n){
+           while(!q.empty() && q.back()<nums[j]){
+               q.pop_back();
+           }
+           q.push_back(nums[j]);
+           if(j-i+1 < k){
+               j++;
+           }
+           else if(j-i+1 ==k){
+               ans.push_back(q.front());
+               if(nums[i]==q.front()){
+                   q.pop_front();
+                   
+               }
+               i++;
+               j++;
+           }
+       }
+       return ans;        
+    }
+};
